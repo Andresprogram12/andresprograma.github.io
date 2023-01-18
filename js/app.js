644d3ce2dd1b7da14f4d788fd91a9a85b3b53abd
Andresprogram12 <121470309+Andresprogram12@users.noreply.github.com>
2023-01-17 19:13:59 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const hamburguesa = document.querySelector('.hamburguesa');
+const navegacion = document.querySelector('.navegacion'); 
+const enlaces = document.querySelectorAll('.navegacion a');
+const fecha = document.querySelector('.fecha');
+
+document.addEventListener('DOMContentLoaded',()=>{
+    mostrarMenu();
+    cerrarMenu();
+    fechaActual();
+});
+
+function mostrarMenu(){
+    hamburguesa.addEventListener('click',()=>{
+        navegacion.classList.toggle('ocultar');
+    });
+}
+
+function cerrarMenu(){
+    enlaces.forEach(enlace =>{
+        enlace.addEventListener('click',(e)=>{
+           
+           if(e.target.tagName === 'A'){
+            navegacion.classList.add('ocultar');
+           }
+        });
+    })
+}
+
+
+
+function fechaActual(){
+    let fechaHoy = new Date().getFullYear();
+    fecha.textContent = fechaHoy;
+}
